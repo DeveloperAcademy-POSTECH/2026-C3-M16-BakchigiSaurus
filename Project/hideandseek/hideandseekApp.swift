29cd85f0654b69213524d0cfea74aee9486f49e4
LanaKee <55011525+LanaKee@users.noreply.github.com>
2026-05-28 11:47:48 +0900

    chore(initialize): create xcode project with certificate

---
+++ Project/hideandseek/hideandseekApp.swift
@@ -0,0 +1,17 @@
+//
+//  hideandseekApp.swift
+//  hideandseek
+//
+//  Created by Lanakee on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct hideandseekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
