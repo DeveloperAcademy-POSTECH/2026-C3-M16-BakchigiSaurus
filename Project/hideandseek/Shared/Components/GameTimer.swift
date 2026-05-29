1e0f06f598f651f8ccf1b44cb4aaac4d22f3cb47
LanaKee <55011525+LanaKee@users.noreply.github.com>
2026-05-29 11:58:33 +0900

    feat: GameTimer display formatted time based on seconds

--- Project/hideandseek/Shared/Components/GameTimer.swift
+++ Project/hideandseek/Shared/Components/GameTimer.swift
@@ -8,21 +8,33 @@
 import SwiftUI
 
 struct GameTimer: View {
-    var body: some View {
-      ZStack {
-        Rectangle()
-          .foregroundStyle(.black)
-          .frame(maxWidth: 180, maxHeight: 67)
-          .cornerRadius(33)
-        Text("10:00")
-          .font(Font.largeTitle.bold())
-      }
+  // TimeLeft in seconds
+  let timeLeft: Int
+  
+  // format seconds to MM:SS
+  private var formattedTime: String {
+    let minutes = timeLeft / 60
+    let seconds = timeLeft % 60
+    return String(format: "%02d:%02d", minutes, seconds)
+  }
+
+  var body: some View {
+    ZStack {
+      Rectangle()
+        .foregroundStyle(.black)
+        .frame(maxWidth: 180, maxHeight: 67)
+        .cornerRadius(33)
+      Text(formattedTime)
+        .font(Font.largeTitle.bold())
     }
+  }
 }
 
 #Preview {
   VStack {
-    GameTimer()
+    GameTimer(timeLeft: 600) // 10:00
+    GameTimer(timeLeft: 65)  // 01:05
+    GameTimer(timeLeft: 9)  // 00:09
   }
   .padding(24)
   .background(.white)
