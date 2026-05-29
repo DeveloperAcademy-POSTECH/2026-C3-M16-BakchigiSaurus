6b255c983673f9e3a11a5254627683c237c04bf6
LanaKee <55011525+LanaKee@users.noreply.github.com>
2026-05-29 11:54:20 +0900

    feat: implement basic ui of  GameTimer component

---
+++ Project/hideandseek/Shared/Components/GameTimer.swift
@@ -0,0 +1,27 @@
+//
+//  GameTimer.swift
+//  hideandseek
+//
+//  Created by Lanakee on 5/29/26.
+//
+
+import SwiftUI
+
+struct GameTimer: View {
+    var body: some View {
+      ZStack {
+        Rectangle()
+          .foregroundStyle(.black)
+          .frame(maxWidth: 180, maxHeight: 67)
+          .cornerRadius(33)
+        Text("10:00")
+          .font(Font.largeTitle.bold())
+      }
+    }
+}
+
+#Preview {
+  VStack {
+    GameTimer()
+  }.padding(24)
+}
