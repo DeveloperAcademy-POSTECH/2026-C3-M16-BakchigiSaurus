15896cd918bc2c2b3c3bfd81570de88c96875b3e
kamcho01 <sjjy08@gmail.com>
2026-06-01 10:25:12 +0900

    design: Alert 스타일 변경
    기본 제공하는 스타일에서 글라스 스타일로 변경 중입니다.

--- Project/hideandseek/Features/Game/Tagger/View/HintActivationView.swift
+++ Project/hideandseek/Features/Game/Tagger/View/HintActivationView.swift
@@ -12,18 +12,28 @@ struct HintActivationView: View {
     @Binding var isAlertPresented: Bool
     
     var body: some View {
-        EmptyView()
-            .alert("힌트를 사용할까요?", isPresented: $isAlertPresented) {
-                Button("네", role: .none) {
-                    //TODO: HintSuccessView Or HintFailureView 로 이동함
+        
+        if isAlertPresented {
+            ZStack {
+                Color.black.opacity(0.5)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        //is AlertPresented = false
+                    }
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("힌트를 사용할까요?")
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundColor(.white)
+                    Text("가장 가까운 사람의 방향이 잠시동안 표시됩니다")
+                        .font(.system(size: 16))
+                        .foregroundColor(.white.opacity(0.8))
+                        .lineSpacing(4)
                 }
-                Button("아니요", role: .cancel) { }
-            } message: {
-                Text("가장 가까운 사람의 방향이 잠시동안 표시됩니다")
             }
+        }
     }
 }
-
-#Preview {
-    HintActivationView(isAlertPresented: .constant(true))
-}
+    
+    #Preview {
+        HintActivationView(isAlertPresented: .constant(true))
+    }
