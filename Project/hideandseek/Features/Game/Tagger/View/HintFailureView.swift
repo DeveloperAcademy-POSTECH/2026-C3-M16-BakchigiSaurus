bfbfe3165100685efd58c8f4d4ebf46913f20b6a
kamcho01 <sjjy08@gmail.com>
2026-06-02 11:11:27 +0900

    feat/ Hint-Failure-View 카메라 추가

--- Project/hideandseek/Features/Game/Tagger/View/HintFailureView.swift
+++ Project/hideandseek/Features/Game/Tagger/View/HintFailureView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct HintFailureView: View {
+    
+    let camera: CameraModel
+    let isHiderNearby: Bool
+    let isUsingHint: Bool
+    
     var body: some View {
         ZStack {
-//            TODO: CameraView 호출
+            GameCameraBackground(camera: camera,
+                                isRevealed: isHiderNearby && isUsingHint,
+                                isRecording: isHiderNearby)
             Color.appDanger
                 .ignoresSafeArea()
                 .opacity(0.75)
@@ -55,5 +62,5 @@ struct HintFailureView: View {
 }
 
 #Preview {
-    HintFailureView()
+    HintFailureView(camera: CameraModel(), isHiderNearby: false, isUsingHint: false)
 }
