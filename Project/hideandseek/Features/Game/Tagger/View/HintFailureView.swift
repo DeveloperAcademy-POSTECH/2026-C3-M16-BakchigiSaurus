d062fa0e5b4f50f4c840b4259b28f21b860c6be6
kamcho01 <sjjy08@gmail.com>
2026-06-02 10:02:48 +0900

    feat/ 힌트 실패 시 화면 구현

--- Project/hideandseek/Features/Game/Tagger/View/HintFailureView.swift
+++ Project/hideandseek/Features/Game/Tagger/View/HintFailureView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct HintFailureView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+//            TODO: CameraView 호출
+            Color.appDanger
+                .ignoresSafeArea()
+                .opacity(0.75)
+            ZStack {
+                VStack {
+                    GameTimer(timeLeft: 300)
+                    Spacer()
+                    Image(systemName: "xmark")
+                        .font(.system(size: 200))
+                    Spacer()
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("화살표 방향에")
+                                .font(.largeTitle.bold())
+                                .foregroundStyle(.secondary)
+                            HStack {
+                                Text("숨은 사람")
+                                    .font(.largeTitle.bold())
+                                    .foregroundStyle(.primary)
+                                Text("이 없어요")
+                                    .font(.largeTitle.bold())
+                                    .foregroundStyle(.secondary)
+                            }
+                            Button {
+
+                            } label: {
+                                Label("힌트 (n개 남음)", systemImage: "magnifyingglass")
+                                    .padding(.vertical, 10)
+                                    .font(.title3)
+                            }
+                            .buttonStyle(.glass)
+                            .cornerRadius(20)
+                            .padding(.bottom, 7)
+                            .opacity(0)
+                            .disabled(true)
+                        }
+                        Spacer()
+                    }
+                }
+            }
+            .padding(.horizontal, 24)
+        }
     }
 }
 
