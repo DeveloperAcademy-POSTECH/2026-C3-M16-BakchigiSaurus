ceb7ac2b2d162168e6e9e7ab3cbdf3f39024ed54
hazi0930 <karin1203@naver.com>
2026-06-02 14:37:47 +0900

    feat: NI 기본구조 및 상태, 에러 정의

--- Project/hideandseek/NearbyInteraction/NearbyInteractionManager.swift
+++ Project/hideandseek/NearbyInteraction/NearbyInteractionManager.swift
@@ -5,32 +5,55 @@
 //  Created by 허지우 on 5/29/26.
 //
 
-import SwiftUI
+// import SwiftUI
+import Foundation
 import NearbyInteraction
 
 
-final class NearbyInteractionManager: NSObject, NISessionDelegate {
-    private var session: NISession?
+final class NearbyInteractionManager: NSObject {
+    var session: NISession?
     
-    func start() {
-        session = NISession()
-        session?.delegate = self
+    private(set) var state: NearbyInteractionState = .idle
+    private(set) var sharedTokenWithPeer = false
+    
+    // 초기화 함수
+    override init() {
+        super.init()
     }
     
+    // NI 세션을 시작 준비하는 함수
+    func startSession() {
+        guard NISession.deviceCapabilities.supportsPreciseDistanceMeasurement else {
+            state = .unsupported
+            return
+        }
+        
+        // NI 세션 생성
+        let newSession = NISession()
+        newSession.delegate = self
+        
+        session = newSession
+        sharedTokenWithPeer = false
+        state = .ready
+    }
+    
+    // NISession에서 내 token 가져오기
     func getMyDiscoveryToken() -> NIDiscoveryToken? {
         return session?.discoveryToken
     }
     
-    func run(with peerToken: NIDiscoveryToken) {
-        let config = NINearbyPeerConfiguration(peerToken: peerToken)
-        session?.run(config)
+    // 세션 종료 함수
+    func invalidateSession() {
+        session?.invalidate()
+        session = nil
+        sharedTokenWithPeer = false
+        state = .invalidated
     }
-    
-    
 }
 
 
-
-// #Preview {
-//    NearbyInteractionManager()
-// }
+extension NearbyInteractionManager: NISessionDelegate {
+    func session(_ session: NISession, didUpdate nearbyObjects: [NINearbyObject]) {
+        // 추후 브랜치에서 보완 예정
+    }
+}
