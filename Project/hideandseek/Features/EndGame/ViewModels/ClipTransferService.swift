dc53c92b979b7d3d643a4d43aec8a80fb636b8f8
LEE CHIHOON <dlclgns152615@gmail.com>
2026-06-04 20:19:29 +0900

    style: format code style

--- Project/hideandseek/Features/EndGame/ViewModels/ClipTransferService.swift
+++ Project/hideandseek/Features/EndGame/ViewModels/ClipTransferService.swift
@@ -19,25 +19,25 @@ final class ClipTransferService {
         case done
         case failed
     }
-    
+
     /// (호스트) 참가자별로 수집된 클립.
     private(set) var collected: [PeerID: [URL]] = [:]
     /// (참가자) 내 클립 전송 상태.
     private(set) var sendState: SendState = .idle
-    
+
     private let transport: any ClipTransport
-    
+
     init(transport: any ClipTransport) {
         self.transport = transport
     }
-    
+
     /// (호스트) 도착하는 클립을 참가자별로 수집한다. View의 `.task`에서 호출.
     func collectIncoming() async {
         for await clip in transport.makeIncomingClipStream() {
             collected[clip.from, default: []].append(clip.url)
         }
     }
-    
+
     /// (참가자) 내 클립들을 호스트에게 순차 전송한다.
     func send(_ clips: [URL], to host: PeerID) async {
         sendState = .sending
