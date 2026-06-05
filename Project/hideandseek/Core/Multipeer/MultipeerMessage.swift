502c28534d5c2c7c9930fdf4442dd68ed5617c9d
LanaKee <55011525+LanaKee@users.noreply.github.com>
2026-06-05 10:16:22 +0900

    style: fix code style

--- Project/hideandseek/Core/Multipeer/MultipeerMessage.swift
+++ Project/hideandseek/Core/Multipeer/MultipeerMessage.swift
@@ -13,7 +13,7 @@ import Foundation
 
 /// MCSession을 통해 주고받는 메시지 종류.
 /// 수신 측은 kind를 보고 payload를 어떤 방식으로 해석할지 결정한다.
-enum MultipeerMessageKind: String, Sendable {
+enum MultipeerMessageKind: String {
     case niDiscoveryToken
 }
 
@@ -40,7 +40,7 @@ extension MultipeerMessageKind: Codable {
 
 /// MCSession data 전송에 사용하는 공통 메시지 래퍼.
 /// kind는 메시지 종류를 나타내고, payload에는 실제 전송할 Data가 들어간다.
-struct MultipeerMessage: Sendable {
+struct MultipeerMessage {
     let kind: MultipeerMessageKind
     let payload: Data
 }
