e0cc939e320779b0628e588120c1ecb63528e19d
garasani <garasani10@gmail.com>
2026-06-07 21:49:34 +0900

    feat/Hider모드
    숨은사람 플로우(스케치) 구현

--- Project/hideandseek/Features/Game/Hider/Models/HiderModeState.swift
+++ Project/hideandseek/Features/Game/Hider/Models/HiderModeState.swift
@@ -8,11 +8,8 @@
 import Foundation
 
 enum HiderModeState {
-    case idle // 시작 전
-    case hiding // 숨는 중
+    case hiding // 숨는 중. 술래 탐지 중
     case taggerNearby // 술래가 가까움, 경고 표시
     case recording // 카메라 켜지고 녹화됨
-    case taggedCheck // 술래에게 잡혔나요? 화면
-    case tagged // 최종적으로 잡힘
-    case gameEnded // 게임 종료
+    case taggedCheck // 술래에게 잡혔는지 확인 화면
 }
